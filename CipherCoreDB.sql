05bbb87612070a851367106f38faf2cefab31ea4
Mustafa Ceylan <mustafa.cya21@icloud.com>
2026-01-02 17:43:28 +0300

    Modify user_keys and messages tables for encryption
    Updated user_keys and messages tables to use BLOB for encryption-related fields and added new columns for key management.

--- CipherCoreDB.sql
+++ CipherCoreDB.sql
@@ -15,20 +15,11 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
+    public_key BLOB NOT NULL,
+    private_key BLOB NOT NULL,
+    key_salt BLOB NOT NULL,
+    key_iv BLOB NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- ==============================
--- USER KEYS (Asimetrik Anahtarlar)
--- ==============================
-CREATE TABLE IF NOT EXISTS user_keys (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    public_key TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
 );
 
 -- ==============================
@@ -38,9 +29,11 @@ CREATE TABLE IF NOT EXISTS messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     sender_id INT NOT NULL,
     receiver_id INT NOT NULL,
-    encrypted_message TEXT NOT NULL,
-    encrypted_sym_key TEXT NOT NULL,
-    signature TEXT NOT NULL,
+    encrypted_message BLOB NOT NULL,
+    encrypted_aes_key BLOB NOT NULL,
+    iv BLOB NOT NULL,
+    message_hash BINARY(32) NOT NULL,
+    signature BLOB NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (sender_id)
         REFERENCES users(id)
@@ -49,31 +42,3 @@ CREATE TABLE IF NOT EXISTS messages (
         REFERENCES users(id)
         ON DELETE CASCADE
 );
-
--- In addition, these are the codes I used to work on tables
-
-ALTER TABLE users
-ADD public_key BLOB,
-ADD private_key BLOB;
-
-ALTER TABLE users
-ADD key_salt BLOB,
-ADD key_iv BLOB;
-
-ALTER TABLE messages
-CHANGE encrypted_sym_key encrypted_aes_key BLOB NOT NULL;
-
-ALTER TABLE messages
-ADD COLUMN iv BLOB NOT NULL,
-ADD COLUMN message_hash BINARY(32) NOT NULL;
-
--- SELECT * FROM users;
--- SELECT * FROM messages;
--- SHOW CREATE TABLE messages;
-
-ALTER TABLE messages
-MODIFY encrypted_message BLOB NOT NULL,
-MODIFY encrypted_aes_key BLOB NOT NULL,
-MODIFY iv BLOB NOT NULL,
-MODIFY signature BLOB NOT NULL;
-
